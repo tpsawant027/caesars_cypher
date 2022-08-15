416007b1d31bb734a5eede1e0165d228b6cca8d5
arroeldo <tejassawant027@gmail.com>
2022-08-15 16:08:02 +0530

    first commit

---
+++ encrypter_caesar.py
@@ -0,0 +1,19 @@
+# caesars cypher encoder
+def encrypter(message: str,offset: int):
+    lst = [chr(i) for i in range(97,123)]
+    message = message.lower()
+    enmsg = ""
+    lst2 = message.split()
+    for ele in lst2:
+        for i in range(len(ele)):
+            if ele[i] in lst:
+                indx = lst.index(ele[i])
+                enmsg+=lst[(indx - offset)]
+            else:
+                enmsg+=ele[i]
+        print(enmsg, end = " ")
+        enmsg = ""
+
+
+if __name__ == '__main__':
+    encrypter("Hello World!", 5)
